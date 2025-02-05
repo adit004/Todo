c607273900ebeed2f3b97a506fdc79b22cc2dd8b
adi-t02 <kadithya129@gmail.com>
2025-02-05 14:45:40 +0530

    second

--- new_app/urls.py
+++ new_app/urls.py
@@ -3,5 +3,6 @@ from django.urls import path
 from new_app import views
 
 urlpatterns = [
-path('',views.index,name="index")
+path('',views.index,name="index"),
+path('dashboard',views.dashboard,name="dashboard")
 ]
